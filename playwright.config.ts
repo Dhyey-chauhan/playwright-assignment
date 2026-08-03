7df5d3c009fe1bab4cb9af81fce4e8adf3f977ec
Dhyey-chauhan <dhyeychauhan165@gmail.com>
2026-08-03 17:28:34 +0530

    chore: update Playwright configuration and add new reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,31 @@
 import { defineConfig, devices } from '@playwright/test';
+import 'dotenv/config';
 
 export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
   retries: 0,
   workers: 1,
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html', { open: 'never' }],
+    ['@testdino/playwright', {
+      token: process.env.TESTDINO_TOKEN,
+      serverUrl: process.env.TESTDINO_SERVER_URL,
+    }],
+  ],
   use: {
     trace: 'on',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
     headless: false,
+  },
+  expect: {
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.01,
+      threshold: 0.2,
+      animations: 'disabled',
+    },
   },
   projects: [
     {
